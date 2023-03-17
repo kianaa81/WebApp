9ea33ade8a0f21ce2db12206a963d5ab48343c23
Kiana <salmasikiana@gmail.com>
2023-03-17 15:05:37 -0600

    Set the useState to false and added some other lines of code to hide the results box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [showFinalResults, setFinalResults] = useState(false);
+
   return (
     <div className="App">
       <h1>Harry Potter Quiz</h1>
       <h2>Current score: 4</h2>
-      <div className='questions'>
+
+      {showFinalResults ? (
+              <div className='results'>
+              <h1>Final Results</h1>
+              <h2>3 out of 5 correct</h2>
+    
+              <button>Play Again</button>
+          </div>
+        ) : (
+        <div className='questions'>
         <h2>Questions 4 out of 5</h2>
 
         <h3 className='questionTxt'>How many Harry Potter books are there?</h3>
@@ -16,14 +29,7 @@ function App() {
           <li>7 Books</li>
         </ul>
       </div>
-
-      <div className='results'>
-          <h1>Final Results</h1>
-          <h2>3 out of 5 correct</h2>
-
-          <button>Play Again</button>
-      </div>
-
+        ) }
     </div>
   );
 }
